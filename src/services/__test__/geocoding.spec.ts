1b42ac452cfb4f43431d81060577fe7c52bf2c27
Chee Kian Teoh <teoh4770@mylaurier.ca>
2025-09-13 19:14:23 -0400

    fix regression test by clearing out mocks before each test

--- src/services/__test__/geocoding.spec.ts
+++ src/services/__test__/geocoding.spec.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it, vi} from "vitest";
+import {beforeEach, describe, expect, it, vi} from "vitest";
 import {fetchPlaceData, geocodeLocations} from "../geocoding.ts";
 import axios from "axios";
 
@@ -14,6 +14,10 @@ const mockedAxios = vi.mocked(axios, {
 });
 
 describe('fetchPlaceData', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    })
+
     it('should fetch and transform place data successfully', async () => {
         // Arrange
         const searchQuery = 'Berlin';
@@ -40,6 +44,75 @@ describe('fetchPlaceData', () => {
                 name: 'Berlin',
                 latitude: 52.52437,
                 longitude: 13.41053,
+            }
+        ]
+
+        mockedAxios.get.mockResolvedValueOnce(mockApiResponse)
+
+        // Act
+        const result = await fetchPlaceData(searchQuery);
+
+        // Assert
+        expect(mockedAxios.get).toHaveBeenCalledOnce();
+        expect(mockedAxios.get).toHaveBeenCalledWith(
+            'https://geocoding-api.open-meteo.com/v1/search',
+            {params: {name: searchQuery}}
+        )
+
+        expect(result).toEqual(expectedTransformedData);
+    });
+
+    it('should filter out places with missing coordinates', async () => {
+        // Arrange
+        const searchQuery = 'Test-data';
+        const mockApiResponse = {
+            data: {
+                results: [
+                    {
+                        id: 2950159,
+                        name: "Berlin",
+                        latitude: 52.52437,
+                        longitude: 13.41053,
+                        elevation: 74,
+                        feature_code: "PPLC",
+                        country_code: "DE",
+                        country: "Germany"
+                    },
+                    {
+                        id: 2643743,
+                        name: "London",
+                        latitude: 51.50853,
+                        // longitude is missing!
+                        elevation: 25,
+                        feature_code: "PPLC",
+                        country_code: "GB",
+                        country: "United Kingdom"
+                    },
+                    {
+                        id: 2988507,
+                        name: "Paris",
+                        latitude: 48.85341,
+                        longitude: 2.3488,
+                        elevation: 42,
+                        feature_code: "PPLC",
+                        country_code: "FR",
+                        country: "France"
+                    }
+                ]
+            },
+            status: 200
+        }
+
+        const expectedTransformedData = [
+            {
+                name: 'Berlin',
+                latitude: 52.52437,
+                longitude: 13.41053,
+            },
+            {
+                name: 'Paris',
+                latitude: 48.85341,
+                longitude: 2.3488,
             }
         ]
 
